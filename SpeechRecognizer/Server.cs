3c162db2dcd171ce3e037fbbeea445aabc375f9b
weswigham <wwigham@gmail.com>
2014-01-16 19:36:46 -0500

    Abstracted server away into its own class, SpeechServer new inherits that
    Also found where System.Web.Script.Serialization was hiding! In System.Web.Extensions!
    It's more useful when converting JSON into indeterminate (read: dynamic) types.
    It's also just plain easier to use; no mucking about with streams needed.
    Benchmarks do show that the DataContractJsonSerializer is faster, though;
    it also does verification for us. So for finalized classes, it is `probably`
    better.

---
+++ SpeechRecognizer/Server.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+//using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Mycroft.App.Message;
+using System.Web.Script.Serialization; //This can be used with dynamic, unlike the contract
+
+namespace Mycroft.App
+{
+    public abstract class Server
+    {
+        private string manifest;
+        private TcpClient cli;
+        private Stream stream;
+        private StreamWriter writer;
+        private Encoding enc = new UTF8Encoding(true, true);
+        private JavaScriptSerializer ser = new JavaScriptSerializer();
+        private StreamReader reader;
+        
+        public string InstanceId;
+
+        public Server()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var textStreamReader = new StreamReader(assembly.GetManifestResourceStream("SpeechRecognizer.app_manifest.json"));
+            manifest = textStreamReader.ReadToEnd();
+            var jsobj = ser.Deserialize<dynamic>(manifest);
+            InstanceId = jsobj["instanceId"];
+        }
+        public void connect(string hostname, string port)
+        {
+            cli = new TcpClient(hostname, Convert.ToInt32(port));
+            setStream(cli.GetStream());
+            startListening();
+        }
+
+        public void setStream(Stream s)
+        {
+            stream = s;
+            writer = new StreamWriter(s, enc);
+            reader = new StreamReader(s, enc);
+        }
+
+        private void Response(MessageType type, dynamic jsonobj)
+        {
+            return;
+        }
+
+        private void Response(string badtype, dynamic jsonobj)
+        {
+            //This probably doesn't need to throw an error... a log might be sufficient.
+            throw new ArgumentException("Invalid message type "+badtype+" recieved!");
+        }
+
+        public async Task startListening()
+        {
+            await sendManifest();
+            while (true)
+            {
+                dynamic obj = await readJson();
+                string type = obj.type;
+                dynamic message = obj.message;
+
+                switch (type)
+                {
+                case "APP_MANIFEST": 
+                    {
+                        Response(new APP_MANIFEST(), message);
+                        break;
+                    }
+                case "APP_MANIFEST_OK": 
+                    {
+                        Response(new APP_MANIFEST_OK(), message);
+                        break;
+                    }
+                case "APP_MANIFEST_FAIL": 
+                    {
+                        Response(new APP_MANIFEST_FAIL(), message);
+                        break;
+                    }
+                case "APP_DEPENDENCY":
+                    {
+                        Response(new APP_DEPENDENCY(), message);
+                        break;
+                    }
+                case "MSG_QUERY":
+                    {
+                        Response(new MSG_QUERY(), message);
+                        break;
+                    }
+                case "MSG_QUERY_SUCCESS":
+                    {
+                        Response(new MSG_QUERY_SUCCESS(), message);
+                        break;
+                    }
+                case "MSG_QUERY_FAIL":
+                    {
+                        Response(new MSG_QUERY_FAIL(), message);
+                        break;
+                    }
+                case "MSG_BROADCAST":
+                    {
+                        Response(new MSG_BROADCAST(), message);
+                        break;
+                    }
+                case "MSG_BROADCAST_SUCCESS":
+                    {
+                        Response(new MSG_BROADCAST_SUCCESS(), message);
+                        break;
+                    }
+                case "MSG_BROADCAST_FAIL":
+                    {
+                        Response(new MSG_BROADCAST_FAIL(), message);
+                        break;
+                    }
+                default:
+                    {
+                        Response(type, message);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        public async void closeConnection()
+        {
+            await sendData("APP_DOWN", "");
+            cli.Close();
+        }
+
+        public async Task sendData(string type, string data)
+        {
+            string msg = type + " " + data;
+            string composition = enc.GetBytes(msg).Length.ToString() + "\n" + msg;
+            await writer.WriteLineAsync(composition);
+            writer.Flush();
+        }
+
+        public async Task sendJson(string type, Object o)
+        {            
+            string obj = ser.Serialize(o);
+            string msg = type + " " + obj;
+            await writer.WriteLineAsync(enc.GetBytes(msg).Length.ToString() + "\n" + msg);
+            writer.Flush();
+        }
+
+        public async Task sendManifest()
+        {
+            await sendData("APP_MANIFEST", manifest);
+        }
+
+        public async Task<Object> readJson()
+        {
+            //Size of message in bytes
+            string len = reader.ReadLine();
+            var size = Convert.ToInt32(len);
+
+            //buffer to put message in
+            var buf = new Char[size];
+
+            //Get the message
+            await reader.ReadBlockAsync(buf, 0, size);
+            var str = Convert.ToString(buf);
+            var re = new Regex(@"^([A-Z_]*)");
+
+            //Match the message type
+            var match = re.Match(str);
+            if (match.Length <= 0)
+            {
+                throw new ArgumentException("Couldn't match a message type string in message: " + str);
+            }
+
+            //Convert the json string to an object
+            var jsonstr = str.TrimStart(match.Value.ToCharArray());
+            if (jsonstr.Trim().Length == 0)
+            {
+                return new
+                {
+                    type = match.Value,
+                    message = new { }
+                };
+            }
+            var obj = ser.Deserialize<dynamic>(jsonstr);
+
+            //Return the type string and the object
+            return new
+            {
+                type = match.Value,
+                message = obj
+            };
+        }
+
+    }
+}
--- SpeechRecognizer/SpeechServer.cs
+++ SpeechRecognizer/SpeechServer.cs
@@ -8,13 +8,13 @@ using System.IO;
 using System.Reflection;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.Serialization.Json;
 using Microsoft.CSharp.RuntimeBinder;
 using System.Speech.Recognition;
 using System.Globalization;
 using System.Speech.Recognition.SrgsGrammar;
 using System.Xml;
 using System.Speech.AudioFormat;
+using Mycroft.App.Message;
 
 namespace SpeechRecognizer
 {
@@ -51,45 +51,19 @@ namespace SpeechRecognizer
         }
     }
     
-    public class SpeechServer
+    public class SpeechServer : Mycroft.App.Server
     {
-        private string manifest;
-        private TcpClient cli;
-        private NetworkStream stream;
-        private StreamWriter writer;
-        private Encoding enc = new UTF8Encoding(true, true);
-        private DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(object));
-        private StreamReader reader;
 
         private Dictionary<string, SpeechRecognitionEngine> sres;
         private Dictionary<string, CombinedGrammar> grammars;
 
-        public string instanceId;
-
-        public SpeechServer()
+        public SpeechServer() : base()
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            var textStreamReader = new StreamReader(assembly.GetManifestResourceStream("SpeechRecognizer.app_manifest.json"));
-            manifest = textStreamReader.ReadToEnd();
-            instanceId = ((dynamic)ser.ReadObject(new MemoryStream(Encoding.UTF8.GetBytes(manifest)))).instanceId;
-
             sres = new Dictionary<string, SpeechRecognitionEngine>();
             grammars = new Dictionary<string, CombinedGrammar>();
         }
 
-        public void connect(string hostname, string port)
-        {
-            cli = new TcpClient(hostname, Convert.ToInt32(port));
-            setStream(cli.GetStream());
-            startListening();
-        }
-
-        public void setStream(Stream s)
-        {
-            writer = new StreamWriter(s, enc);
-            reader = new StreamReader(s, enc);
-        }
-
+        
         public void addGrammar(string name, string xml)
         {
             var gram = new CombinedGrammar(name, xml);
@@ -150,123 +124,79 @@ namespace SpeechRecognizer
             return new NegotiatedAudioStream(new MemoryStream(), 44100, AudioBitsPerSample.Sixteen, AudioChannel.Mono);
         }
 
-        private delegate void response_del(SpeechServer self, dynamic message);
-
-        private Dictionary<string,response_del> responses = new Dictionary<string,response_del>{
-            {"APP_MANIFEST_OK", (self, message) => 
+        
+        private void Response(APP_MANIFEST_OK _, dynamic message)  
+        {
+            InstanceId = message.instanceId;
+            return;
+        }
+        private void Response(APP_DEPENDENCY _, dynamic message)  
+        {
+            try
+            {
+                dynamic mics = message.microphone;
+                var ups =  new List<string>();
+                foreach (FieldInfo fieldinfo in mics.GetType().GetFields())
                 {
-                    self.instanceId = message.instanceId;
-                    return;
-                }
-            },
-            {"APP_MANIFEST_FAIL", (self, message) => 
-                {
-                    return;
-                }
-            },
-            {"APP_DEPENDENCY", (self, message) => 
-                {
-                    try
+                    string inst = fieldinfo.Name;
+                    string stat = (string)fieldinfo.GetValue(mics);
+                    if (stat == "up")
                     {
-                        dynamic mics = message.microphone;
-                        var ups =  new List<string>();
-                        foreach (FieldInfo fieldinfo in mics.GetType().GetFields())
+                        ups.Add(inst);
+                    }
+                }
+                if (ups.Count > 0)
+                {
+                    Up(ups);
+                }
+                else
+                {
+                    Down();
+                }
+            }
+            catch (RuntimeBinderException)
+            {
+                Down();
+            }
+        }
+            
+        private void Response(MSG_QUERY _, dynamic message)  
+        {
+            switch ((string)message.action)
+            {
+                case "load_grammar":
+                    {
+                        foreach (FieldInfo fieldinfo in message.data.grammar.GetType().GetFields())
                         {
                             string inst = fieldinfo.Name;
-                            string stat = (string)fieldinfo.GetValue(mics);
-                            if (stat == "up")
-                            {
-                                ups.Add(inst);
-                            }
+                            string stat = (string)fieldinfo.GetValue(message.data.grammar);
+                            addGrammar(inst, stat);
                         }
-                        if (ups.Count > 0)
-                        {
-                            self.Up(ups);
-                        }
-                        else
-                        {
-                            self.Down();
-                        }
+                        sendJson("MSG_QUERY_SUCCESS", new {ret = new {}});
+                        break;
                     }
-                    catch (RuntimeBinderException)
+                default:
                     {
-                        self.Down();
+                        break;
                     }
-                }
-            },
-            { "MSG_QUERY", (self, message) => 
-                {
-                    switch ((string)message.action)
-                    {
-                        case "load_grammar":
-                            {
-                                foreach (FieldInfo fieldinfo in message.data.grammar.GetType().GetFields())
-                                {
-                                    string inst = fieldinfo.Name;
-                                    string stat = (string)fieldinfo.GetValue(message.data.grammar);
-                                    self.addGrammar(inst, stat);
-                                }
-                                self.sendJson("MSG_QUERY_SUCCESS", new {ret = new {}});
-                                break;
-                            }
-                        default:
-                            {
-                                break;
-                            }
-                    }
-                }
-            },
-            { "MSG_QUERY_SUCCESS", (self, message) =>
-                {
-                    return;
-                }
-            },
-            { "MSG_QUERY_FAIL", (self, message) =>
-                {
-                    return;
-                }
-            },
-            { "MSG_BROADCAST", (self, message) =>
-                {
-                    try
-                    {
-                        self.removeGrammar(message.content.unloadGrammar);
-                        var obj = new {message = "Success"};
-                        self.sendJson("MSG_BROADCAST_SUCCESS", obj);
-                    }
-                    catch (RuntimeBinderException)
-                    {
-                        //Didn't have a grammar removal request
-                    }
-
-                    return;
-                }
-            },
-            { "MSG_BROADCAST_SUCCESS", (self, message) =>
-                {
-                    return;
-                }
-            },
-            { "MSG_BROADCAST_FAIL", (self, message) =>
-                {
-                    return;
-                }
             }
-        };
+        }
+         
 
-        public async Task startListening()
+        private void Response(MSG_BROADCAST _, dynamic message)
         {
-            await sendManifest();
-            while (true) {
-                dynamic obj = await readJson();
-                string type = obj.type;
-                dynamic message = obj.message;
-
-                if (responses.ContainsKey(type)) 
-                {
-                    responses[type](this, message);
-                }
+            try
+            {
+                removeGrammar(message.content.unloadGrammar);
+                var obj = new {message = "Success"};
+                sendJson("MSG_BROADCAST_SUCCESS", obj);
             }
+            catch (RuntimeBinderException)
+            {
+                //Didn't have a grammar removal request
+            }
+
+            return;
         }
 
         public void Up(List<string> instances)
@@ -287,74 +217,5 @@ namespace SpeechRecognizer
             }
         }
 
-        public async void closeConnection()
-        {
-            await sendData("APP_DOWN","");
-            cli.Close();
-        }
-
-        public async Task sendData(string type, string data)
-        {
-            string msg = type + " " + data;
-            string composition = enc.GetBytes(msg).Length.ToString() + "\n" + msg;
-            await writer.WriteLineAsync(composition);
-            writer.Flush();
-        }
-
-        public async Task sendJson(string type, Object o)
-        {
-            Stream s = new MemoryStream();
-            ser.WriteObject(s,o);
-            var reads = new StreamReader(s, enc);
-            string obj = reads.ReadToEnd();
-            string msg = type + " " + obj;
-            await writer.WriteLineAsync(enc.GetBytes(msg).Length.ToString() + "\n" + msg);
-            writer.Flush();
-        }
-
-        public async Task sendManifest()
-        {
-            await sendData("APP_MANIFEST", manifest);
-        }
-
-        public async Task<Object> readJson()
-        {
-            //Size of message in bytes
-            string len = reader.ReadLine();
-            var size = Convert.ToInt32(len);
-            
-            //buffer to put message in
-            var buf = new Char[size]; 
-
-            //Get the message
-            await reader.ReadBlockAsync(buf, 0, size);
-            var str = Convert.ToString(buf);
-            var re = new Regex(@"^([A-Z_]*)");
-
-            //Match the message type
-            var match = re.Match(str);
-            if (match.Length <= 0)
-            {
-                throw new ArgumentException("Couldn't match a message type string in message: " + str);
-            }
-
-            //Convert the json string to an object
-            var jsonstr = str.TrimStart(match.Value.ToCharArray());
-            if (jsonstr.Trim().Length == 0)
-            {
-                return new { 
-                    type = match.Value,
-                    message = new { }
-                };
-            }
-            var jsonmem = new MemoryStream(enc.GetBytes(jsonstr));
-            var obj = ser.ReadObject(jsonmem);
-
-            //Return the type string and the object
-            return new {
-                type = match.Value, 
-                message = obj
-            };
-        }
     }
 }
